8a4a419a5904eee186eee178f1f38bc2c59a4596
adhocmaster <adhocmaster@live.com>
2023-03-08 13:26:11 -0800

    my-agent

--- kits/python/agentMuki.py
+++ kits/python/agentMuki.py
@@ -2,12 +2,17 @@ from lux.kit import obs_to_game_state, GameState, EnvConfig
 from lux.utils import direction_to, my_turn_to_place_factory
 import numpy as np
 import sys
+import logging
 class Agent():
     def __init__(self, player: str, env_cfg: EnvConfig) -> None:
         self.player = player
         self.opp_player = "player_1" if self.player == "player_0" else "player_0"
         np.random.seed(0)
         self.env_cfg: EnvConfig = env_cfg
+        self.logger = logging.getLogger(self.player)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
 
     def early_setup(self, step: int, obs, remainingOverageTime: int = 60):
         if step == 0:
